843234c8d129c2324408774f04bd73d992d1f352
Jexy <165495525+Jexxy1517@users.noreply.github.com>
2024-10-11 20:22:02 +0700

    Update login.php

--- UTSLec/Registration_page/login.php
+++ UTSLec/Registration_page/login.php
@@ -4,40 +4,31 @@
 
 session_start();
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
-   $name = mysqli_real_escape_string($conn, $_POST['name']);
-   $email = mysqli_real_escape_string($conn, $_POST['email']);
-   $pass = md5($_POST['password']);
-   $cpass = md5($_POST['cpassword']);
-   $user_type = $_POST['user_type'];
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = md5($_POST['password']);
 
-   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+    $select = "SELECT * FROM user_form WHERE email = '$email' AND password = '$pass'";
+    $result = mysqli_query($conn, $select);
 
-   $result = mysqli_query($conn, $select);
+    if (mysqli_num_rows($result) > 0) {
 
-   if(mysqli_num_rows($result) > 0){
+        $row = mysqli_fetch_array($result);
 
-      $row = mysqli_fetch_array($result);
-
-      if($row['user_type'] == 'admin'){
-
-         $_SESSION['admin_name'] = $row['name'];
-         header('location:admin.php');
-
-      }elseif($row['user_type'] == 'user'){
-
-         $_SESSION['user_name'] = $row['name'];
-         header('location:user.php');
-
-      }
-     
-   }else{
-      $error[] = 'incorrect email or password!';
-   }
-
-};
+        if ($row['user_type'] == 'admin') {
+            $_SESSION['admin_name'] = $row['name'];
+            header('location:admin.php');
+        } elseif ($row['user_type'] == 'user') {
+            $_SESSION['user_name'] = $row['name'];
+            header('location:user.php');
+        }
+    } else {
+        $error[] = 'Incorrect email or password!';
+    }
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
