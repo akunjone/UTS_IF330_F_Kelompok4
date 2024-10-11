d2e1930113974fee5e0e23f9369b24d47cb8699a
Jexy <165495525+Jexxy1517@users.noreply.github.com>
2024-10-11 20:22:28 +0700

    Update register.php

--- UTSLec/Registration_page/register.php
+++ UTSLec/Registration_page/register.php
@@ -2,37 +2,32 @@
 
 @include 'config.php';
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
-   $name = mysqli_real_escape_string($conn, $_POST['name']);
-   $email = mysqli_real_escape_string($conn, $_POST['email']);
-   $pass = md5($_POST['password']);
-   $cpass = md5($_POST['cpassword']);
-   $user_type = $_POST['user_type'];
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = md5($_POST['password']);
+    $cpass = md5($_POST['cpassword']);
+    $user_type = $_POST['user_type'];
 
-   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+    $select = "SELECT * FROM user_form WHERE email = '$email'";
 
-   $result = mysqli_query($conn, $select);
+    $result = mysqli_query($conn, $select);
 
-   if(mysqli_num_rows($result) > 0){
-
-      $error[] = 'user already exist!';
-
-   }else{
-
-      if($pass != $cpass){
-         $error[] = 'password not matched!';
-      }else{
-         $insert = "INSERT INTO user_form(name, email, password, user_type) VALUES('$name','$email','$pass','$user_type')";
-         mysqli_query($conn, $insert);
-         header('location:login_form.php');
-      }
-   }
-
-};
-
-
+    if (mysqli_num_rows($result) > 0) {
+        $error[] = 'User already exists!';
+    } else {
+        if ($pass != $cpass) {
+            $error[] = 'Passwords do not match!';
+        } else {
+            $insert = "INSERT INTO user_form(name, email, password, user_type) VALUES('$name','$email','$pass','$user_type')";
+            mysqli_query($conn, $insert);
+            header('location:login.php');
+        }
+    }
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
