93e431b46019caea8862e7133fe6844ff74382f2
Ric4j4 <richardwijaya31@gmail.com>
2024-10-21 16:48:32 +0700

    Create home.php

---
+++ UTSWEBPROG/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../UTSWEBPROG/style.css">
+    <title>Madevent</title>
+</head>
+<body>
+    <header>
+        <nav class="NavbarComponents">
+            <h1 class="NavbarSymbol">Madevent</h1>
+            <ul>
+                <li><a class="NavbarMenu" href="../UTSWEBPROG/home.php"><i class="fa-solid fa-house-chimney"></i>Home</a></li>
+                <li><a class="NavbarMenu" href="../UTSWEBPROG/registration.php"><i class="fa-solid fa-tree"></i>SignUp</a></li>
+                <li><a class="NavbarMenu" href="../UTSWEBPROG/login.php"><i class="fa-solid fa-tree"></i>Login</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
