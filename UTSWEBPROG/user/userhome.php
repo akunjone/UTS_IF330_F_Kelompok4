10314e4b491bba0789fe12502c0b9d36b8af5ea2
Jexy <165495525+Jexxy1517@users.noreply.github.com>
2024-10-22 04:00:11 +0700

    Update userhome.php

--- UTSWEBPROG/user/userhome.php
+++ UTSWEBPROG/user/userhome.php
@@ -1,22 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="../user/styleuser.css">
+    <link rel="stylesheet" href="../UTSWEBPROG/style.css">
     <title>Madevent</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column; 
+            justify-content: flex-start; 
+            align-items: center;
+            margin: 0;
+            background-color: #0d1b2a;
+            font-family: Arial, sans-serif;
+            color: #ffffff;
+        }
+        .NavbarComponents {
+            width: 100%;
+            background-color: #1b263b;
+            padding: 10px 20px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+            position: fixed; 
+            top: 0;
+            left: 0;
+            z-index: 1000; 
+        }
+        .NavbarSymbol {
+            display: inline-block;
+            font-size: 24px;
+            color: #00d9ff;
+        }
+        .NavbarMenu {
+            color: #ffffff;
+            text-decoration: none;
+            margin: 0 15px;
+            display: inline-block;
+        }
+        .NavbarMenu:hover {
+            text-decoration: underline;
+        }
+        .content {
+            margin-top: 150px;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <header>
-        <nav class="NavbarComponentsUser">
-            <h1 class="NavbarSymbolUser">Madevent</h1>
-            <ul>
-                <li><a class="NavbarMenuUser" href="../user/userhome.php"><i class="fa-solid fa-house-chimney"></i>Home</a></li>
-                <li><a class="NavbarMenuUser" href="../user/event.php"><i class="fa-solid fa-info"></i>View Events</a></li>
-                <li><a class="NavbarMenuUser" href="../user/registevent.php"><i class="fa-solid fa-tree"></i>Event Registration</a></li>
-                <li><a class="NavbarMenuUser" href="../user/viewregistered.php"><i class="fa-solid fa-tree"></i>Registered</a></li>
-                <li><a class="NavbarMenuUser" href="../user/logout.php"><i class="fa-solid fa-tree"></i>Logout</a></li>
-                <li><a class="NavbarMenuUser" href="../user/profilemanagement.php"><i class="fa-solid fa-tree"></i>Profile</a></li>
-            </ul>
+        <nav class="NavbarComponents">
+            <h1 class="NavbarSymbol">Madevent</h1>
+            <div>
+                <a class="NavbarMenu" href="../user/userhome.php"><i class="fa-solid fa-house-chimney"></i>Home</a>
+                <a class="NavbarMenu" href="../user/event.php"><i class="fa-solid fa-info"></i>View Events</a>
+                <a class="NavbarMenu" href="../user/registevent.php"><i class="fa-solid fa-tree"></i>Event Registration</a>
+                <a class="NavbarMenu" href="../user/viewregistered.php"><i class="fa-solid fa-tree"></i>Registered</a>
+                <a class="NavbarMenu" href="../user/logout.php"><i class="fa-solid fa-tree"></i>Logout</a>
+                <a class="NavbarMenu" href="../user/profilemanagement.php"><i class="fa-solid fa-tree"></i>Profile</a>
+            </div>
         </nav>
     </header>
+    <div class="content">
+        <h1>Welcome to Madevent!</h1>
+        <p>SLEEP -> OPTIONAL</p>
+        <p>EAT -> OPTIONAL</p>
+        <p>PLAY -> NEEDED</p>
+        <p>DIE -> MAYBE</p>
+    </div>
 </body>
 </html>
