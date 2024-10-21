5b144adaeb5e167e5beabfa723bb62634e4e67d1
Ric4j4 <richardwijaya31@gmail.com>
2024-10-21 16:53:43 +0700

    Create userhome.php

---
+++ UTSWEBPROG/user/userhome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../user/styleuser.css">
+    <title>Madevent</title>
+</head>
+<body>
+    <header>
+        <nav class="NavbarComponentsUser">
+            <h1 class="NavbarSymbolUser">Madevent</h1>
+            <ul>
+                <li><a class="NavbarMenuUser" href="/"><i class="fa-solid fa-house-chimney"></i>Home</a></li>
+                <li><a class="NavbarMenuUser" href="../user/event.php"><i class="fa-solid fa-info"></i>View Events</a></li>
+                <li><a class="NavbarMenuUser" href="../user/registevent.php"><i class="fa-solid fa-tree"></i>Event Registration</a></li>
+                <li><a class="NavbarMenuUser" href="../user/viewregistered.php"><i class="fa-solid fa-tree"></i>Registered</a></li>
+                <li><a class="NavbarMenuUser" href="../user/logout.php"><i class="fa-solid fa-tree"></i>Logout</a></li>
+                <li><a class="NavbarMenuUser" href="../user/profilemanagement.php"><i class="fa-solid fa-tree"></i>Profile</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
