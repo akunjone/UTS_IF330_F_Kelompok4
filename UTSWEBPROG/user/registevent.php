53568e7c3d5552a98217160d1f733836bd331179
Ananda-Madhi <147123700+Ananda-Madhi@users.noreply.github.com>
2024-10-23 21:36:15 +0700

    Update registevent.php

--- UTSWEBPROG/user/registevent.php
+++ UTSWEBPROG/user/registevent.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="../user/styleuser.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Event Registration</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../user/styleuser.css">
     <style>
         body {
             display: flex;
@@ -40,84 +44,64 @@
         }
         .content {
             margin-top: 150px;
-            text-align: center;
+            padding: 20px;
+            width: 100%;
         }
-        .table-style{
-            border-collapse: collapse;
-            margin: 25px 0;
-            font-size: 0.9em;
-            font-family: sans-serif;
-            min-width: 400px;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+        .card-deck {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
         }
-        .table-style thead tr {
-            background-color: #45b6d6;
+        .card {
+            margin: 15px;
+            background-color: #1b263b;
             color: #ffffff;
-            text-align: left;
+            border: 1px solid #45b6d6;
         }
-        .table-style th,
-        .table-style td{
-            padding: 12px 15px;
+        .card-img-top {
+            max-width: 100%;
+            height: auto;
         }
-        .table-style tbody tr {
-            border-bottom: 1px solid #dddddd;
-        }
-        .table-style tbody tr:last-of-type {
-            border-bottom: 2px solid #009879;
+        .btn-primary {
+            margin-top: 10px;
         }
     </style>
 </head>
-    <header>
-        <nav class="NavbarComponents">
-            <h1 class="NavbarSymbol">Madevent</h1>
-            <div>
-                <a class="NavbarMenu" href="../user/userhome.php"><i class="fa-solid fa-house-chimney"></i>Home</a>
-                <a class="NavbarMenu" href="../user/event.php"><i class="fa-solid fa-info"></i>View Events</a>
-                <a class="NavbarMenu" href="../user/registevent.php"><i class="fa-solid fa-tree"></i>Event Registration</a>
-                <a class="NavbarMenu" href="../user/viewregistered.php"><i class="fa-solid fa-tree"></i>Registered</a>
-                <a class="NavbarMenu" href="../user/logout.php"><i class="fa-solid fa-tree"></i>Logout</a>
-                <a class="NavbarMenu" href="../user/profilemanagement.php"><i class="fa-solid fa-tree"></i>Profile</a>
-            </div>
-        </nav>
-    </header>
+<header>
+    <nav class="NavbarComponents">
+        <h1 class="NavbarSymbol">Madevent</h1>
+        <div>
+            <a class="NavbarMenu" href="../user/userhome.php">Home</a>
+            <a class="NavbarMenu" href="../user/event.php">View Events</a>
+            <a class="NavbarMenu" href="../user/registevent.php">Event Registration</a>
+            <a class="NavbarMenu" href="../user/viewregistered.php">Registered</a>
+            <a class="NavbarMenu" href="../user/logout.php">Logout</a>
+            <a class="NavbarMenu" href="../user/profilemanagement.php">Profile</a>
+        </div>
+    </nav>
+</header>
 <body>
     <div class="content">
         <h1>Event Registration</h1>
-
-        <table class="table-style">
-            <thead>
-                <tr>
-                    <th>Nama Event</th>
-                    <th>Tanggal</th>
-                    <th>Waktu</th>
-                    <th>Lokasi</th>
-                    <th>Deskripsi</th>
-                    <th>Kapasitas</th>
-                    <th>Registration</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $koneksi = mysqli_connect("localhost", "root", "", "event");
-                $data = mysqli_query($koneksi, "SELECT * FROM events");
-                while ($display = mysqli_fetch_array($data)){
-                    echo "
-                    <tr>
-                        <td>{$display['NamaEvent']}</td>
-                        <td>{$display['Tanggal']}</td>
-                        <td>{$display['Waktu']}</td>
-                        <td>{$display['Lokasi']}</td>
-                        <td>{$display['Deskripsi']}</td>
-                        <td>{$display['Kapasitas']}</td>
-                        <td>
-                            <a href='edit.php?EventID={$display['EventID']}'>Regist</a>
-                        </td>
-                    </tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+        <div class="card-deck">
+            <?php
+            $koneksi = mysqli_connect("localhost", "root", "", "event");
+            $data = mysqli_query($koneksi, "SELECT * FROM events");
+            while ($display = mysqli_fetch_array($data)){
+                echo "
+                <div class='card' style='width: 18rem;'>
+                    <img src='../uploads/{$display['Foto']}' class='card-img-top' alt='{$display['NamaEvent']}'>
+                    <div class='card-body text-center'>
+                        <h5 class='card-title'>{$display['NamaEvent']}</h5>
+                        <a href='edit.php?EventID={$display['EventID']}' class='btn btn-primary'>Register</a>
+                    </div>
+                </div>";
+            }
+            ?>
+        </div>
     </div>
 
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
