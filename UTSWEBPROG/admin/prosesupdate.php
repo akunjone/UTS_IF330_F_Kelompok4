c5c409b6217a882392fee50b92d1b08323c8a49a
Ric4j4 <richardwijaya31@gmail.com>
2024-10-24 18:50:17 +0700

    Create prosesupdate.php

---
+++ UTSWEBPROG/admin/prosesupdate.php
@@ -0,0 +1,42 @@
+<?php
+
+define('HOSTNAME', 'localhost');
+define('MYSQLUSER', 'root');
+define('MYSQLPASS', '');
+define('MYSQLDB', 'event');
+
+if ($koneksi = new PDO('mysql:host=' . HOSTNAME . ';dbname=' . MYSQLDB, MYSQLUSER, MYSQLPASS)) {
+
+    $id = $_POST['id'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
+
+    $query = "UPDATE users
+              SET username = :username, 
+                  email = :email, 
+                  password = :hashed_pass
+              WHERE id = :id";
+
+    $statement = $koneksi->prepare($query);
+
+    $statement->bindParam(':username', $username);
+    $statement->bindParam(':email', $email);
+    $statement->bindParam(':hashed_pass', $hashed_pass);
+    $statement->bindParam(':id', $id);
+
+    if ($statement->execute()) {
+        if ($statement->rowCount() > 0) {
+            header("Location: ../admin/profile.php");
+            exit();
+        } else {
+            echo "No rows updated<br />";
+        }
+    }
+} else {
+    echo "Connection failed.";
+}
+
+$koneksi = null;
+?>
